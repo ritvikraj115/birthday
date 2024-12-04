b1f299a6706508a77a3b287cba2cc88e2b4f355a
ritvikraj115 <143361673+ritvikraj115@users.noreply.github.com>
2024-12-05 00:19:23 +0530

    Update WebCall.js

--- voice-chat/src/Components/WebCall.js
+++ voice-chat/src/Components/WebCall.js
@@ -243,7 +243,7 @@ function WebCall({id}) {
     if(count==0){
     try {
       let i =0;
-      const response = await axios.post('http://localhost:5000/api/create-web-call', {
+      const response = await axios.post('http://dfdfd/api/create-web-call', {
         agentId: id,
         apiKey: 'key_f1e876fda6c185f802ce70ee39df'
       });
